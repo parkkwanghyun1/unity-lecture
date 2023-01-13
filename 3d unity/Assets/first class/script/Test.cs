849f32a47c1b31cc2743e58f8cb14b35f218cd1a
parkkwanghyun1 <110909549+parkkwanghyun1@users.noreply.github.com>
2023-01-13 21:46:06 +0900

    2022.01.13 키입력

--- 3d unity/Assets/first class/script/Test.cs
+++ 3d unity/Assets/first class/script/Test.cs
@@ -12,16 +12,47 @@ class Monster
 public class Test : MonoBehaviour
 {
     public int value = 100;
+
+    public int[] count;
+    public int[] data = new int[3];
+    
     void Start()
     {
-        Debug.Log(value);
-        Monster monster = new Monster();
         
+        Monster monster = new Monster();
+
+        transform.position = new Vector3(0, transform.position.y, 1);
+
+        // transform.postion 은 읽기 전용
+
+        foreach(var element in data)
+        {
+            Debug.Log(element);
+        }
     }
 
-  
     void Update()
     {
-        
+        // GetKeyDown 키를 눌렀을 때
+        // Gekey : 키를 누르고 있을 때 
+        // GetKeyup: 키를 땟을 때 
+
+      
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
     }
 }
