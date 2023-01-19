11215a2232b2496004084c3ae8cc107b74000c7a
parkkwanghyun1 <110909549+parkkwanghyun1@users.noreply.github.com>
2023-01-19 21:46:04 +0900

    2023.0.19
    animation blend

---
+++ 3d unity/Assets/Blend/Script/Motion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Input.GetAxis = -1 ~ 0 ~ 1 사이의 값을 반환한다.
+
+        float vertical = Input.GetAxis("Vertical"); // GetAxis 키입력 감지
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Horizontal", horizontal);
+    }
+}
